b61e1236593b7a8888622403fa8a33724d8a0c75
foleya <foley.ah@gmail.com>
2020-03-31 16:04:28 -0400

    played around with file organization

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,15 +1,19 @@
 import React from "react"
 
-import Header from "../header"
-import Footer from "../footer"
+import Header from "../header/header"
+import Footer from "../footer/footer"
 
 import Styles from "./layout.module.scss"
+
+import "./global.css"
 
 const Layout = props => {
   return (
     <div className={Styles.Container}>
-      <Header />
-      {props.children}
+      <div className={Styles.Content}>
+        <Header />
+        {props.children}
+      </div>
       <Footer />
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-
-import Styles from "../styles/Index.module.scss"
+import Layout from "../components/layout/layout"
 
 const IndexPage = () => {
   return (
     <Layout>
-      <h1 className={Styles.Something}>Hello</h1>
+      <h1>Hello</h1>
       <h2>I'm Dewey</h2>
       <p>
         Contact me by clicking <Link to="/contact">here</Link>
